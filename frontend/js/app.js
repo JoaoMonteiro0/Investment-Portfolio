357ea084712b78be0f0b5e6b67ce0ebc6b0c3d4f
João Monteiro <joaomontero07@gmail.com>
2026-04-17 13:41:04 +0100

    footer fix & folders change
    - Changed the css location of the components
    - Horizontal footer

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,10 +1,1 @@
-fetch("components/navbar.html")
-  .then((res) => {
-    console.log(res.text);
 
-    return res.text();
-  })
-  .then((html) => {
-    console.log(html);
-    document.getElementById("navbar").innerHTML = html;
-  });
